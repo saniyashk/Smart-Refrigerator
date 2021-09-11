575d65418be6de7374b10ced5848ae46028db79a
Saniya <saniya682shk@gmail.com>
2021-09-11 12:55:00 +0530

    Initial commit

---
+++ conf.py
@@ -0,0 +1,20 @@
+#configuration for email
+MAILGUN_API_KEY = '542e56e1f27b3f26f0544109fa5f6cb2-4b1aa784-8830ec84'
+SANDBOX_URL= 'sandboxb8f97102762d49eba24568a0933bfccb.mailgun.org'
+SENDER_EMAIL = 'test@sandboxb8f97102762d49eba24568a0933bfccb.mailgun.org' # No need to modify this. The sandbox URL is of the format test@YOUR_SANDBOX_URL
+RECIPIENT_EMAIL = 'saniya682shk@gmail.com'
+API_KEY = '19677d7c-a2e1-4e5e-a409-6aa9406298ca'
+DEVICE_ID = 'BOLT8022122'
+
+
+
+#configuration for sending sms
+SSID = 'ACa8ac38937179668376233b9a0449831e'
+AUTH_TOKEN = '6a12c8abfcb93816ad8bf9f3002cf5db'
+FROM_NUMBER = '+13608417873'
+TO_NUMBER = '+917738884634'
+FRAME_SIZE = 10
+MUL_FACTOR =6
+
+
+
